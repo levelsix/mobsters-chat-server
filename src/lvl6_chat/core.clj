0d3f045ddbe3eafa83a36d0f238be7d3211088b3
Rangel <raspasov@gmail.com>
2015-04-15 22:36:58 -0700

    initial commit

---
+++ src/lvl6_chat/core.clj
@@ -0,0 +1,7 @@
+(ns lvl6-chat.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/lvl6_chat/core_test.clj
@@ -0,0 +1,7 @@
+(ns lvl6-chat.core-test
+  (:require [clojure.test :refer :all]
+            [lvl6-chat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
